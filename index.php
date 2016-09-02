3de9d34376d9165ebf6f389325ca1f33dbc38f02
Tim <timzhong@Tims-MacBook-Pro.local>
2016-09-02 14:20:56 -0400

    landing page visual effect

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>Spent</title>
+    <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
   </head>
@@ -12,17 +13,28 @@
     
     <div id="step3" class="steps">
     <p onclick="next(3);">next</p>
-    <p onclick="prev(3)">prev</p>
+    <p onclick="prev(3);">prev</p>
     </div>
 
     <div id="step2" class="steps">
-    <p onclick="next(2);">next</p>
-    <p onclick="prev(2)">prev</p>
+        <div class="cwidth">
+            <div class="navbar">
+                <i class="fa fa-caret-left" aria-hidden="true"></i>
+            </div>
+        </div>
     </div>
 
     <div id="step1" class="steps">
-    <p onclick="next(1);">next</p>
-    <p onclick="prev(1)">prev</p>
+        <div id="header">
+            <h1>Spent</h1>
+        </div>
+        <div id="users">
+            <div class="user_buttons" onclick="input_user(0)">T</div>
+            <div class="user_buttons" onclick="input_user(1)">J</div>
+            <div class="user_buttons" onclick="input_user(2)">
+                <i class="fa fa-adjust" aria-hidden="true"></i>
+            </div>
+        </div>
     </div>
 
     <script src="js/jquery.js"></script>
--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+userid = -1;
+
 function next(n){
 	if(n > 0 && n < 3){
 
 		var id = "step" + n;
 		var layer = gid(id);
 
-		layer.style.transform = "translateX(-100%)";
+		layer.style.transform = "translateX(5%)";
+		setTimeout(function(){
+			layer.style.transform = "translateX(-100%)";
+		}, 200);
 	}
 }
 
@@ -15,6 +20,17 @@ function prev(n){
 		var id = "step" + n;
 		var layer = gid(id);
 
-		layer.style.transform = "translateX(0)";
+		layer.style.transform = "translateX(3%)";
+
+		setTimeout(function(){
+			layer.style.transform = "translateX(0)";
+		}, 400);
 	}
+}
+
+function input_user(n){
+	userid = n;
+	setTimeout(function(){
+		next(1);
+	},100)
 }
