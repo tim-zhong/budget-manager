cb423cbafe11d0b7e0ddc9ef314d372720bb4ed1
Runbiao Zhong <timzhong2374@hotmail.co.uk>
2016-09-03 11:48:06 -0400

    font-end 1.0 finished

--- index.php
+++ index.php
@@ -10,17 +10,71 @@
     <link href="css/style.css" rel="stylesheet">
   </head>
   <body>
+    <div id="nav_bar" class="nav_bar_hide">
+            <div class="cwidth">
+                <a id="nav_button" onclick="prev(2);">
+                    <i class="fa fa-caret-left" aria-hidden="true"></i>
+                </a>
+            </div>
+            <a onclick="reset()">
+                <div id="nav_logo">
+                    Spent
+                </div>
+            </a>
+        </div>
     
     <div id="step3" class="steps">
-    <p onclick="next(3);">next</p>
-    <p onclick="prev(3);">prev</p>
+        <div class="cwidth">
+            <div id="form">
+                <div class="input-group">
+                    <span class="input-group-addon">$</span>
+                    <input id="amount" type="number" class="form-control" aria-label="Amount (to the nearest dollar)">
+                </div>
+                <textarea class="form-control" rows="5" id="description" ></textarea>
+            </div>
+            <div id="submit">
+                Done
+            </div>
+        </div>
     </div>
 
     <div id="step2" class="steps">
-        <div class="cwidth">
-            <div class="navbar">
-                <i class="fa fa-caret-left" aria-hidden="true"></i>
+        <div id="user_summary">
+            <div id="greeting">
+                Hi <span id="greeting_username">Tim</span>, you have spent
             </div>
+            <div id="user_summary_amount">1,000.34</div>
+            <div id="since_date">
+                - Since -<br/>
+                24 Dec 2016
+            </div>
+        </div>
+        <div class="cats cwidth">
+            <div class="cat" id="cat1" onclick="next(2)">
+                <i class="fa fa-cutlery" aria-hidden="true"></i>
+                <div class="cat_name">Food</div>
+            </div>
+            <div class="cat" id="cat1" onclick="next(2)">
+                <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                <div class="cat_name">Bills</div>
+            </div>
+            <div class="cat" id="cat1" onclick="next(2)">
+                <i class="fa fa-star" aria-hidden="true"></i>
+                <div class="cat_name">Entertainment</div>
+            </div>
+             <div class="cat" id="cat1" onclick="next(2)">
+               <i class="fa fa-bus" aria-hidden="true"></i>
+                <div class="cat_name">Transportation</div>
+            </div>
+            <div class="cat" id="cat1" onclick="next(2)">
+                <i class="fa fa-shopping-basket" aria-hidden="true"></i>
+                <div class="cat_name">Necessities</div>
+            </div>
+            <div class="cat" id="cat1" onclick="next(2)">
+                <i class="fa fa-question-circle" aria-hidden="true"></i>
+                <div class="cat_name">Other</div>
+            </div>
+            <div clear="both"></div>
         </div>
     </div>
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 userid = -1;
-
+catid = -1;
 function next(n){
 	if(n > 0 && n < 3){
 
@@ -10,23 +10,63 @@ function next(n){
 		setTimeout(function(){
 			layer.style.transform = "translateX(-100%)";
 		}, 200);
+
+		if(n == 1){
+			setTimeout(function(){
+				var navbar = gid("nav_bar");
+				navbar.className = "nav_bar_show";
+			}, 500);
+		}
+
+		if(n == 2){
+			gid("nav_button").onclick=function(){prev(3);};
+		}
 	}
 }
 
 
 function prev(n){
+	if(n == 2){
+		var navbar = gid("nav_bar");
+		navbar.className = "nav_bar_hide";
+	}
+	if(n == 3){
+		gid("nav_button").onclick=function(){prev(2);};
+	}
 	if(n > 1 && n < 4){
-		n = n - 1;
-		var id = "step" + n;
-		var layer = gid(id);
-
-		layer.style.transform = "translateX(3%)";
-
 		setTimeout(function(){
-			layer.style.transform = "translateX(0)";
-		}, 400);
+			n = n - 1;
+			var id = "step" + n;
+			var layer = gid(id);
+
+			layer.style.transform = "translateX(3%)";
+
+			setTimeout(function(){
+				layer.style.transform = "translateX(0)";
+			}, 400);
+		}, 300);
 	}
 }
+function reset(){
+	var navbar = gid("nav_bar");
+	navbar.className = "nav_bar_hide";
+
+	setTimeout(function(){
+		for(var i = 1; i < 4; i++){
+			var id = "step" + i;
+			var layer = gid(id);
+
+			layer.style.transform = "translateX(3%)";
+
+			setTimeout(function(){
+				layer.style.transform = "translateX(0)";
+			}, 400);
+		}
+	}, 300);
+	userid = -1;
+	catid = -1;
+}
+
 
 function input_user(n){
 	userid = n;
